317b22674d4bbbb310d7a28d4594e86ec0d16222
lidu30030 <lidu30030@gmail.com>
2026-02-24 22:47:14 +0800

    Update PhotoScanner.h

--- src/PhotoScanner.h
+++ src/PhotoScanner.h
@@ -4,4 +4,5 @@
  @interface PhotoScanner : DCUniModule
  - (void)ping:(NSDictionary *)options callback:(UniModuleKeepAliveCallback)callback;
  - (void)scan:(NSDictionary *)options callback:(UniModuleKeepAliveCallback)callback;
+ - (void)requestFullAccess:(NSDictionary *)options callback:(UniModuleKeepAliveCallback)callback;
  @end
